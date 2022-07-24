bf5d5beaa7284abb2cc3ea5abdeecfe8dfd99b8e
SmallkeyChen <39688847+SmallkeyChen@users.noreply.github.com>
2022-07-24 21:48:51 +0800

    Update train_model.py

--- CIFAR-10/train_model.py
+++ CIFAR-10/train_model.py
@@ -10,6 +10,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import cifar10
+from models import resnet_layer, resnet_v1, ConvNet_2
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0" 
@@ -22,15 +23,27 @@ if gpus:
     except RuntimeError as e:
         print(e)
     
-       
-
+      
+        
+def lr_schedule(epoch):
+    lr = 1e-3
+    if epoch > 90:
+        lr *= 1e-3
+    elif epoch > 75:
+        lr *= 1e-2
+    elif epoch > 50:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+        
+        
+        
 # hyper-parameters for training resnet-20
 batch_size = 64  
-epochs = 150
+epochs = 100
 data_augmentation = True
 num_classes = 10
 depth = 20
-
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -42,132 +55,12 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 input_shape = x_train.shape[1:]
 
-
-def lr_schedule(epoch):
-    lr = 1e-3
-    if epoch > 130:
-        lr *= 0.5e-3
-    elif epoch > 110:
-        lr *= 1e-3
-    elif epoch > 80:
-        lr *= 1e-2
-    elif epoch > 50:
-        lr *= 1e-1
-    print('Learning rate: ', lr)
-    return lr
-
-
-def resnet_layer(inputs,
-                 num_filters=16,
-                 kernel_size=3,
-                 strides=1,
-                 activation='relu',
-                 batch_normalization=True,
-                 conv_first=True):
-    conv = Conv2D(num_filters,
-                  kernel_size=kernel_size,
-                  strides=strides,
-                  padding='same',
-                  kernel_initializer='he_normal',
-                  kernel_regularizer=l2(1e-4))
-
-    x = inputs
-    if conv_first:
-        x = conv(x)
-        if batch_normalization:
-            x = BatchNormalization()(x)
-        if activation is not None:
-            x = Activation(activation)(x)
-    else:
-        if batch_normalization:
-            x = BatchNormalization()(x)
-        if activation is not None:
-            x = Activation(activation)(x)
-        x = conv(x)
-    return x
-
-
-def resnet_v1(input_shape, depth, num_classes=10):
-    """
-    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
-    Last ReLU is after the shortcut connection.
-    At the beginning of each stage, the feature map size is halved (downsampled)
-    by a convolutional layer with strides=2, while the number of filters is
-    doubled. Within each stage, the layers have the same number filters and the
-    same number of filters.
-    Features maps sizes:
-    stage 0: 32x32, 16
-    stage 1: 16x16, 32
-    stage 2:  8x8,  64
-    The Number of parameters is approx the same as Table 6 of [a]:
-    ResNet20 0.27M
-    ResNet32 0.46M
-    ResNet44 0.66M
-    ResNet56 0.85M
-    ResNet110 1.7M
-
-    # Arguments
-        input_shape (tensor): shape of input image tensor
-        depth (int): number of core convolutional layers
-        num_classes (int): number of classes (CIFAR10 has 10)
-
-    # Returns
-        model (Model): Keras model instance
-    """
-    if (depth - 2) % 6 != 0:
-        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
-    # Start model definition.
-    num_filters = 16
-    num_res_blocks = int((depth - 2) / 6)
-
-    inputs = Input(shape=input_shape)
-    x = resnet_layer(inputs=inputs)
-    # Instantiate the stack of residual units
-    for stack in range(3):
-        for res_block in range(num_res_blocks):
-            strides = 1
-            if stack > 0 and res_block == 0:  # first layer but not first stack
-                strides = 2  # downsample
-            y = resnet_layer(inputs=x,
-                             num_filters=num_filters,
-                             strides=strides)
-            y = resnet_layer(inputs=y,
-                             num_filters=num_filters,
-                             activation=None)
-            if stack > 0 and res_block == 0:  # first layer but not first stack
-                # linear projection residual shortcut connection to match
-                # changed dims
-                x = resnet_layer(inputs=x,
-                                 num_filters=num_filters,
-                                 kernel_size=1,
-                                 strides=strides,
-                                 activation=None,
-                                 batch_normalization=False)
-            x = keras.layers.add([x, y])
-            x = Activation('relu')(x)
-        num_filters *= 2
-
-    # Add classifier on top.
-    # v1 does not use BN after last shortcut connection-ReLU
-    x = AveragePooling2D(pool_size=8)(x)
-    y = Flatten()(x)
-    outputs = Dense(num_classes,
-                    activation='softmax',
-                    kernel_initializer='he_normal')(y)
-
-    # Instantiate model.
-    model = Model(inputs=inputs, outputs=outputs)
-    return model
-
-
-
 model = resnet_v1(input_shape=input_shape, depth=depth)
 model.compile(loss='categorical_crossentropy',
               optimizer=Adam(lr=lr_schedule(0)),
               metrics=['accuracy'])
 
 print(model.summary())
-
 
 # Prepare model model saving directory.
 save_dir = os.path.join(os.getcwd(), 'saved_models')
