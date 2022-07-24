0a9a82a62d66f96e5da31332770bc6c3f2cbef70
SmallkeyChen <39688847+SmallkeyChen@users.noreply.github.com>
2022-07-24 22:05:15 +0800

    Update models.py

--- SVHN/models.py
+++ SVHN/models.py
@@ -1,1 +1,102 @@
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from tensorflow.keras.layers import AveragePooling2D, Input, Flatten
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.models import Model
 
+ 
+def Lenet5(input_shape=(32, 32, 1)):
+    input_tensor = keras.layers.Input(shape=input_shape)
+    
+    x = keras.layers.Convolution2D(6, (5, 5), activation='relu', padding='same', name='block1_conv1')(input_tensor)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2), name='block1_pool1')(x)
+
+    x = keras.layers.Convolution2D(16, (5, 5), activation='relu', padding='same', name='block2_conv1')(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2), name='block2_pool1')(x)
+    
+    x = keras.layers.Flatten(name='flatten')(x)
+    x = keras.layers.Dense(120, activation='relu', name='fc1')(x)
+    x = keras.layers.Dense(84, activation='relu', name='fc2')(x)
+    x = keras.layers.Dense(10, name='before_softmax')(x)
+    x = keras.layers.Activation('softmax', name='redictions')(x)
+    
+    return keras.models.Model(input_tensor, x)
+  
+  
+
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+  
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+
+
+def resnet_v1(input_shape, depth, num_classes=10):
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, 44)')
+
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
+
+    inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs)
+    # Instantiate the stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:  
+                strides = 2  
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters,
+                             strides=strides)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters,
+                             activation=None)
+            if stack > 0 and res_block == 0:  
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+            x = Activation('relu')(x)
+        num_filters *= 2
+
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model  
+  
